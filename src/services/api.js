61a21bac23ee7b0efcb2a83398ecda255197a655
Gabriel Lima <gabriel.lima@aluno.fapce.edu.br>
2019-09-02 11:23:55 -0300

    Adicionando corpo de função para interceptor para verificar o token

--- src/services/api.js
+++ src/services/api.js
@@ -7,6 +7,18 @@ const api = axios.create({
   baseURL: 'http://192.168.111.21:3000'
 });
 
+// Add a response interceptor
+api.interceptors.response.use(function (response) {
+  
+  // console.tron.log("DEU CERTO");
+  return response;
+  
+}, function (error) {
+
+  // console.tron.log("DEU ERRO", error.response);
+  return Promise.reject(error);
+});
+
 api.interceptors.request.use(async (config) => {
   try {
     const token = await AsyncStorage.getItem('@Todo:token');
