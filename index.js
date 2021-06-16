b44263939f7edb61f482fd1f48c4059ff1de4889
Gabriel Lima <gabriel.lima@aluno.fapce.edu.br>
2021-06-16 11:57:31 -0300

    Macbook

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
   console.tron = Reactotron
-  .configure({ host: '192.168.111.38' })
+  .configure({ host: '192.168.1.4' })
   .useReactNative()
   .connect();
 }
